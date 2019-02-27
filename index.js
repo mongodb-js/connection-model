54bd0bc5be56a1385d734f711eacf70793a7e230
aherlihy <anna.herlihy@10gen.com>
2019-02-27 16:56:02 +0100

    add extended connection model

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 module.exports = require('./lib/model');
 module.exports.connect = require('./lib/connect');
+
+module.exports.ExtendedConnectionModel = require('./lib/extended-connection');
+module.exports.ConnectionCollection = require('./lib/connection-collection');
+module.exports.SelectableCollectionMixin = require('./lib/selectable-collection-mixin');
