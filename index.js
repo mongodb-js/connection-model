31d4689d1a115408324ae5f52fe0d7883b896553
aherlihy <anna.herlihy@10gen.com>
2019-02-28 12:32:56 +0100

    PR comments

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = require('./lib/model');
 module.exports.connect = require('./lib/connect');
 
+/* Models used in Compass itself */
 module.exports.ExtendedConnectionModel = require('./lib/extended-connection');
 module.exports.ConnectionCollection = require('./lib/connection-collection');
-module.exports.SelectableCollectionMixin = require('./lib/selectable-collection-mixin');
