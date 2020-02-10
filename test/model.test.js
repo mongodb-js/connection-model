3c962b924a264a70f4c7637f8899ac0125dfcd5f
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2020-02-10 11:55:59 +0100

    add test

--- test/model.test.js
+++ test/model.test.js
@@ -1,7 +1,42 @@
+/* eslint-disable guard-for-in */
 const assert = require('assert');
 const Connection = require('../');
+const _ = require('lodash');
+function flattenObject(o, prefix = '', result = {}, keepNull = true) {
+  if (_.isString(o) || _.isNumber(o) || _.isBoolean(o) || (keepNull && _.isNull(o))) {
+    result[prefix] = o;
+    return result;
+  }
+
+  if (_.isArray(o) || _.isPlainObject(o)) {
+    for (let i in o) {
+      let pref = prefix;
+      if (_.isArray(o)) {
+        pref = pref + `[${i}]`;
+      } else {
+        if (_.isEmpty(prefix)) {
+          pref = i;
+        } else {
+          pref = prefix + '.' + i;
+        }
+      }
+      flattenObject(o[i], pref, result, keepNull);
+    }
+    return result;
+  }
+  return result;
+}
 
 describe('Connection', () => {
+  describe('serialize', () => {
+    it('should not contain any sensitive data', () => {
+      const conn = new Connection({ mongodb_password: 'mypassword' });
+      const result = flattenObject(conn.serialize());
+      for (let key in result) {
+        assert.notEqual(('' + result[key]).includes('mypassword'), true);
+      }
+    });
+  });
   describe('#parse', () => {
     context('when the attributes have legacy passwords', () => {
       context('when the attributes have no new passwords', () => {
