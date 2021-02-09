0be4c9d60ea59977ec928264f13980bcd82699ae
Anemy <rhysh@live.com>
2021-02-09 05:07:40 +0100

    remove unneeded check on ssl method

--- constants/ssl-method-values.js
+++ constants/ssl-method-values.js
@@ -1,27 +1,44 @@
 // Allowed values for the `sslMethod` field
+
+/**
+ * Do not use SSL for anything.
+ */
+const NONE = 'NONE';
+/**
+ * Use system CA.
+ */
+const SYSTEMCA = 'SYSTEMCA';
+/**
+ * Use SSL if available.
+ */
+const IFAVAILABLE = 'IFAVAILABLE';
+/**
+ * Use SSL but do not perform any validation of the certificate chain.
+ */
+const UNVALIDATED = 'UNVALIDATED';
+/**
+ * The driver should validate the server certificate and fail to connect if validation fails.
+ */
+const SERVER = 'SERVER';
+/**
+ * The driver must present a valid certificate and validate the server certificate.
+ */
+const ALL = 'ALL';
+
 module.exports = [
-  /**
-   * Do not use SSL for anything.
-   */
-  'NONE',
-  /**
-   * Use system CA.
-   */
-  'SYSTEMCA',
-  /**
-   * Use SSL if available.
-   */
-  'IFAVAILABLE',
-  /**
-   * Use SSL but do not perform any validation of the certificate chain.
-   */
-  'UNVALIDATED',
-  /**
-   * The driver should validate the server certificate and fail to connect if validation fails.
-   */
-  'SERVER',
-  /**
-   * The driver must present a valid certificate and validate the server certificate.
-   */
-  'ALL'
+  NONE,
+  SYSTEMCA,
+  IFAVAILABLE,
+  UNVALIDATED,
+  SERVER,
+  ALL
 ];
+
+module.exports.SSL_METHODS = {
+  NONE,
+  SYSTEMCA,
+  IFAVAILABLE,
+  UNVALIDATED,
+  SERVER,
+  ALL
+};
